8a4aa5f2ad37a3e353dc8837efcde9b203cd0d54
TezSmith <Montez.Smith1@gmail.com>
2018-06-25 18:03:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,4 +3,56 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i
+    @square = @num * @num
+    "#{@square}"
+  end
+
+  get '/say/:number/:phrase' do
+    answer = ''
+
+    params[:number].to_i.times do
+      answer += params[:phrase]
+    end
+
+    answer
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+
+    @first_word = params[:word1]
+    @second_word = params[:word2]
+    @third_word = params[:word3]
+    @fourth_word = params[:word4]
+    @fifth_word = params[:word5]
+
+    "#{@first_word} #{@second_word} #{@third_word} #{@fourth_word} #{@fifth_word}."
+
+  end
+
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
+  end
+
 end
